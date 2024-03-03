81be5f3fdf315074a468cdea52f976f821c3b4ce
JOJOx1337 <jais2793@gmail.com>
2024-03-03 23:14:08 +0530

    new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,135 @@
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import "./SvgTest.css";
 import gsap from "gsap";
-import {useGSAP} from "@gsap/react"
-import { MotionPathPlugin } from "gsap/all";
+import { useGSAP } from "@gsap/react";
+import { MotionPathPlugin, ScrollTrigger } from "gsap/all";
+
+import smoothscroll from 'smoothscroll-polyfill';
 
 
-function App() {
 
+const SvgTest = () => {
   
-  useGSAP(()=> {
-    gsap.registerPlugin(MotionPathPlugin) 
+  // Initialize the polyfill
+  smoothscroll.polyfill();
+
+ 
+  const [mounted, setMounted] = useState(false);
+  const cloudRef = useRef(null);
+
+  useLayoutEffect(() => {
+    if (mounted) {
+      cloudRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [mounted]);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  
+
+  useGSAP(() => {
+    gsap.registerPlugin(MotionPathPlugin, ScrollTrigger);
     // gsap code here
-    gsap.to(".cloud",{
-      duration:5,
+
+
+
+    // timeline 1
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#path1",
+        start: "top top",
+        end: "bottom center",
+        // end: '+=' + ((window.innerHeight * 3) + 50),
+        scrub: true,
+        markers: true,
+        // onEnter: () => {
+        //   setAbsolute(true)
+        // },
+        // onEnterBack: () => {
+        //   setAbsolute(true)
+        // },
+
+        onEnter: () => {
+          // Remove the fixed positioning and centering styles
+          gsap.set("#cloud", { position: "", top: "", left: "", transform: "", opacity: "1" });
+        },
+
+        onEnterBack: () => {
+          // Remove the fixed positioning and centering styles
+          gsap.set("#cloud", { position: "", top: "", left: "", transform: "" });
+        },
+        onLeave:() => {
+          gsap.set("#cloud", { position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" });
+        }
+
+      },
+    });
+
+
+    tl.to("#cloud", {
+      duration: 1,
       ease: "none",
-      motionPath:{
-        path: ".path",
-        align: ".path",
-        alignOrigin: [0.5, 0.5]
-        
-      }
-    })
-  })
+
+      motionPath: {
+        path: "#path1",
+        align: "#path1",
+        alignOrigin: [0.5, 0.5],
+      },
+    });
+
+    // // timeline 2
+    // let tl2 = gsap.timeline({
+    //   scrollTrigger: {
+    //     trigger: "#path1",
+    //     start: "bottom center",
+    //     end: "bottom center",
+    //     scrub: true,
+    //     markers: true,
+    //     onComplete: () => {
+    //       console.log('second trigger');
+    //     }
+    //   },
+    // });
+
+  });
+
+
 
   return (
     <>
-      <div class="cloud" style={{ background: "black", width: "100px", height: "100px" }}>
-        Cloud
+      {/* */}
+      <div id="track">
+        <nav className="h-32"></nav>
+        <section className="section-1">
+      <img src="src/assets/Cloud.png" alt="Cloud" id="cloud" ref={cloudRef}/>
+          <svg
+            viewBox="0 0 1623 2685"
+            fill="none"
+          // preserveAspectRatio="xMidYMax meet"
+          >
+            <path
+              id="path1"
+              d="M1622.5 0.5C1589.67 150.667 1447.7 511.4 1142.5 753C837.3 994.6 549.667 1117.67 444 1149C345 1183.67 137.1 1275.8 97.5 1367C48 1481 -11.5 1515.5 3.50001 1911.5C15.5 2228.3 332.167 2390.17 489 2431.5C593 2464.5 806.9 2561.2 830.5 2684"
+              stroke="yellow" 
+              stroke-opacity="0.5"
+            />
+
+
+          </svg>
+
+
+
+        </section>
+        <section className="section-2">
+
+        </section>
+        <section className="section-3"></section>
+        <section className="section-4"></section>
+        <section className="section-5"></section>
       </div>
-      <svg viewBox="0 0 1397 1982" fill="none" preserveAspectRatio='xMidYMax meet'>
-        <path className="path" d="M1396 0.5C1214.67 133.833 843.5 428.5 809.5 540.5C775.5 652.5 256.667 1006.5 1.5 1169.5C198.5 1367.83 592.5 1807.9 592.5 1981.5" stroke="red" />
-
-      </svg>
-
     </>
-  )
-}
+  );
+};
 
-export default App
+export default SvgTest;
